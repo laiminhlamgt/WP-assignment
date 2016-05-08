dcd4cc0814b883e601a98684631f6dafd0af5fa7
Minh Lam <laiminhlamgt@gmail.com>
2016-05-08 21:56:01 +0700

    Update header

--- views/header.php
+++ views/header.php
@@ -36,47 +36,45 @@
     <div id="navbarMenuDiv" class="d-navbar-menu d-navbar-menu-collapse">
       <ul class="d-clearfix d-navbar-left">
         <li class="d-menu-haschild">
-          <a class="d-navbar-active" href="#">MUA</a>
+          <!-- <a class="d-navbar-active">MUA</a> -->
+          <a>MUA</a>
           <i class="menu-icon fa fa-plus-circle" onclick="collapseContent('dMenuContent1',this)"></i>
           <div class="d-menu-content" id="dMenuContent1">
             <div class="d-menu-content-header">Mua Nhà Đất</div>
             <div class="d-menu-content-body">
               <div class="d-menu-content-body-child child-1">
                 <h3 class="child-title">Mua Nhà</h3>
-                <a href="#">Nhà Cấp 1</a>
-                <a href="#">Nhà Cấp 2</a>
-                <a href="#">Nhà Cấp 3</a>
-                <a href="#">Nhà Cấp 4</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=9">Căn hộ chung cư</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=10">Nhà riêng</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=11">Nhà mặt phố</a>
               </div>
               <div class="d-menu-content-body-child child-2">
-                <h3 class="child-title">Mua Đất</h3>
-                <a href="#">Đất Cấp 1</a>
-                <a href="#">Đất Cấp 2</a>
-                <a href="#">Đất Cấp 3</a>
-                <a href="#">Đất Cấp 4</a>
+                <h3 class="child-title">Khác</h3>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=12">Kho, nhà xưởng, đất</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=13">Khác</a>
               </div>
             </div>
           </div>
         </li>
         <li class="d-menu-haschild" >
-          <a href="#">BÁN</a>
+          <a>THUÊ</a>
           <i class="menu-icon fa fa-plus-circle" onclick="collapseContent('dMenuContent2',this)"></i>
           <div class="d-menu-content" id="dMenuContent2">
-            <div class="d-menu-content-header">Bán Nhà Đất</div>
+            <div class="d-menu-content-header">Thuê Nhà Đất</div>
             <div class="d-menu-content-body">
               <div class="d-menu-content-body-child child-1">
-                <h3 class="child-title">Bán Nhà</h3>
-                <a href="#">Nhà Cấp 1</a>
-                <a href="#">Nhà Cấp 2</a>
-                <a href="#">Nhà Cấp 3</a>
-                <a href="#">Nhà Cấp 4</a>
+                <h3 class="child-title">Thuê Nhà</h3>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=1">Căn hộ chung cư</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=2">Nhà riêng</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=3">Nhà mặt phố</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=5">Phòng trọ</a>
               </div>
               <div class="d-menu-content-body-child child-2">
-                <h3 class="child-title">Bán Đất</h3>
-                <a href="#">Đất Cấp 1</a>
-                <a href="#">Đất Cấp 2</a>
-                <a href="#">Đất Cấp 3</a>
-                <a href="#">Đất Cấp 4</a>
+                <h3 class="child-title">Khác</h3>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=4">Văn phòng</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=6">Cửa hàng</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=7">Kho, nhà xưởng, đất</a>
+                <a href="<?php echo URL; ?>search/search?stype-of-house=8">Khác</a>
               </div>
             </div>
           </div>
