1c51cdea59887096aecf8336902ea1585a479caf
lapduongbkit <lapduongbkit@gmail.com>
2016-05-08 23:49:51 +0700

    2016 0508 : Duong Tran : Commit for add sign out and front end link in dashboard

--- views/dashboard/top-menu.php
+++ views/dashboard/top-menu.php
@@ -1,7 +1,7 @@
 <div class='d-db-top-menu'>
-	<div class='d-db-top-menu-item'><span><a href="">Front end</a></span></div>
+	<div class='d-db-top-menu-item'><span><a href="<?php echo URL; ?>">Front end</a></span></div>
 	<div class='d-db-top-menu-item'><span><a href="">Clear cache</a></span></div>
 	<div class='d-db-top-menu-item'><img class='d-db-top-menu-logo' src="<?php echo URL; ?>/public/images/logo.png" alt="logo" /></div>
 	<div class='d-db-top-menu-item'><span><a href="">Back up</a></span></div>
-	<div class='d-db-top-menu-item'><span>Hi Admin | <a href="">Logout</a></span></div>
+	<div class='d-db-top-menu-item'><span>Hi Admin | <a href="<?php echo URL . 'login/logout'; ?>">Logout</a></span></div>
 </div>
